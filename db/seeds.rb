f56e83ae06ee7012805be1a633087e8c1e772add
Juyeon Kim <jay.jykim91@gmail.com>
2020-11-17 22:46:50 +0900

    finished but ugly

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "Clearing the Article Database"
+Article.destroy_all
+
+puts "Generating Articles"
+10.times do
+  temp_article = Article.create( title: Faker::Name.name,
+                  content: Faker::Verb.base)
+  puts "Generated #{temp_article.title}"
+end
+
+puts "Generated #{Article.count} articles"
